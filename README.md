51105b59f1bf5897b974b8131fa0ac3844afb45c
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2020-03-04 19:23:30 -0300

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# react-tetris
-Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles.css";
+
+import Game from "./components/Game";
+
+export default function App() {
+  return <Game />;
+}
