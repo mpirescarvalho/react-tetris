48d80ce88fa7fdb2e96a1239a365184bac06302d
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2020-03-05 22:07:14 -0300

    add start page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./styles.css";
 
-import Game from "./components/Game";
+import Tetris from "./components/Tetris";
 
 export default function App() {
-  return <Game />;
+  return <Tetris />;
 }
--- src/components/Stage/index.js
+++ src/components/Stage/index.js
@@ -1,46 +1,46 @@
 import React from "react";
 import styled from "styled-components";
 
-const PIXEL_SIZE = 25;
+// const PIXEL_SIZE = 25;
 
 const Game = styled.div`
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: flex-start;
-  background-color: black;
+	width: 100vw;
+	height: 100vh;
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	align-items: flex-start;
+	background-color: black;
 `;
 
 const Next = styled.div`
-  width: 100px;
-  height: 100px;
-  background-color: black;
-  overflow: hidden;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+	width: 100px;
+	height: 100px;
+	background-color: black;
+	overflow: hidden;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
 `;
 
 const StyledStage = styled.div`
-  width: 50%;
-  height: 100%;
-  background-color: black;
-  overflow: hidden;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+	width: 50%;
+	height: 100%;
+	background-color: black;
+	overflow: hidden;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
 `;
 
 const Row = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  width: 100%;
-  height: 100%;
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	width: 100%;
+	height: 100%;
 `;
 
 const Pixel = React.memo(styled.div`
@@ -52,78 +52,74 @@ const Pixel = React.memo(styled.div`
   justify-content: center;
   align-items: center;
   background-color: ${props =>
-    props.fill === 1
-      ? `rgb(${props.color[0]},${props.color[1]},${props.color[2]})}`
-      : "#aaa"}; */
+			props.fill === 1
+				? `rgb(${props.color[0]},${props.color[1]},${props.color[2]})}`
+				: "#aaa"}; */
 
 
   /* border-radius: 5px; */
   background: ${props =>
-    props.fill === 1
-      ? `rgb(${props.color[0]},${props.color[1]},${props.color[2]}, 0.8)}`
-      : "#aaa"};
+			props.fill === 1
+				? `rgb(${props.color[0]},${props.color[1]},${props.color[2]}, 0.8)}`
+				: "#aaa"};
   border: ${props => (props.fill === 0 ? "4px solid" : "4px solid")};
   border-bottom-color: ${props =>
-    props.fill === 1
-      ? `rgb(${props.color[0]},${props.color[1]},${props.color[2]}, 0.1)}`
-      : "#aaa"};
+			props.fill === 1
+				? `rgb(${props.color[0]},${props.color[1]},${props.color[2]}, 0.1)}`
+				: "#aaa"};
   border-right-color: ${props =>
-    props.fill === 1
-      ? `rgb(${props.color[0]},${props.color[1]},${props.color[2]}, 0.1)}`
-      : "#aaa"};
+			props.fill === 1
+				? `rgb(${props.color[0]},${props.color[1]},${props.color[2]}, 0.1)}`
+				: "#aaa"};
   border-top-color: ${props =>
-    props.fill === 1
-      ? `rgb(${props.color[0]},${props.color[1]},${props.color[2]}, 1)}`
-      : "#aaa"};
+			props.fill === 1
+				? `rgb(${props.color[0]},${props.color[1]},${props.color[2]}, 1)}`
+				: "#aaa"};
   border-left-color: ${props =>
-    props.fill === 1
-      ? `rgb(${props.color[0]},${props.color[1]},${props.color[2]}, 0.3)}`
-      : "#aaa"};
+			props.fill === 1
+				? `rgb(${props.color[0]},${props.color[1]},${props.color[2]}, 0.3)}`
+				: "#aaa"};
 
 `);
 
 const Stage = ({ map, player }) => {
-  return (
-    <Game>
-      {player.next && (
-        <Next>
-          {player.next.bloco.map((row, y) => (
-            <Row key={`row-${y}`}>
-              {row.map((pixel, x) => {
-                return (
-                  <Pixel
-                    key={`pixel-${x}`}
-                    fill={pixel}
-                    color={player.next.color}
-                  />
-                );
-              })}
-            </Row>
-          ))}
-        </Next>
-      )}
-      {map && (
-        <StyledStage>
-          {map.map((row, y) => (
-            <Row key={`row-${y}`}>
-              {row.map((pixel, x) => {
-                let playerFill =
-                  player.bloco.bloco[y - player.pos[0]] &&
-                  player.bloco.bloco[y - player.pos[0]][x - player.pos[1]];
-                return (
-                  <Pixel
-                    key={`pixel-${x}`}
-                    fill={pixel.fill | playerFill}
-                    color={playerFill ? player.bloco.color : pixel.color}
-                  />
-                );
-              })}
-            </Row>
-          ))}
-        </StyledStage>
-      )}
-    </Game>
-  );
+	return (
+		<Game>
+			{player.next && (
+				<Next>
+					{player.next.bloco.map((row, y) => (
+						<Row key={`row-${y}`}>
+							{row.map((pixel, x) => {
+								return (
+									<Pixel key={`pixel-${x}`} fill={pixel} color={player.next.color} />
+								);
+							})}
+						</Row>
+					))}
+				</Next>
+			)}
+			{map && (
+				<StyledStage>
+					{map.map((row, y) => (
+						<Row key={`row-${y}`}>
+							{row.map((pixel, x) => {
+								let playerFill =
+									player.bloco.bloco[y - player.pos[0]] &&
+									player.bloco.bloco[y - player.pos[0]][x - player.pos[1]];
+								return (
+									<Pixel
+										key={`pixel-${x}`}
+										fill={pixel.fill | playerFill}
+										color={playerFill ? player.bloco.color : pixel.color}
+									/>
+								);
+							})}
+						</Row>
+					))}
+				</StyledStage>
+			)}
+		</Game>
+	);
 };
 
 export default Stage;
