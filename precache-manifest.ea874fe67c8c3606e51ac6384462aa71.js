0f58dd47bdd82f1ed8ed93a214a58f6819120243
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2020-03-10 00:16:42 -0300

    Updates

---
+++ precache-manifest.ea874fe67c8c3606e51ac6384462aa71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fae65628a01b0e71b55f473ce5e87aa0",
+    "url": "/react-tetris/index.html"
+  },
+  {
+    "revision": "96aaf40b13af64f944d9",
+    "url": "/react-tetris/static/css/main.1602bf20.chunk.css"
+  },
+  {
+    "revision": "76474decc0e2ad38f701",
+    "url": "/react-tetris/static/js/2.2beafffc.chunk.js"
+  },
+  {
+    "revision": "65f61a1b574cbfdae1aa1e0f7a13f7ca",
+    "url": "/react-tetris/static/js/2.2beafffc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96aaf40b13af64f944d9",
+    "url": "/react-tetris/static/js/main.0793db4a.chunk.js"
+  },
+  {
+    "revision": "64a66e52d4204da78390",
+    "url": "/react-tetris/static/js/runtime-main.4b2a5979.js"
+  },
+  {
+    "revision": "6255a470f96dd4c751f85ff8c63b0172",
+    "url": "/react-tetris/static/media/background.6255a470.jpg"
+  }
+]);
